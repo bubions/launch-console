96e4ca2ad8d06accd2e7f6b93d5af133573c0eeb
bubions <raiyan.y.khan07@gmail.com>
2026-09-11 05:10:28 +0000

    Finished launch console

--- launch_console.py
+++ launch_console.py
@@ -3,6 +3,7 @@ print(f"Welcome to the Launch Console, {user_name}!")
 
 isRunning = True
 while isRunning:
+    print("")
 
     print("1. About me")
     print("2. My goals")
@@ -12,11 +13,17 @@ while isRunning:
     user_choice = input("Pick an option 1-4: ")
 
     if user_choice == "1": # ABOUT ME
-        pass
+        print("I'm Raiyan, a sophmore at Vista Ridge High School.")
     elif user_choice == "2": # MY GOALS
-        pass
+        print("My goal is to ship my first real project this term.")
     elif user_choice == "3": # CAT
-        pass
+        print("  /\\      /\\")
+        print(" /  \\____/  \\")
+        print("| ___   ___ |")
+        print("|/ | \\ / | \\|")
+        print("|\\___/ \\___/|")
+        print("|           |")
+        print("|___________|")
     elif user_choice == "4": # EXIT
         print("Goodbye!")
         isRunning = False
